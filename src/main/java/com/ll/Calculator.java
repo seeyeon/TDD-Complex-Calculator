2618bedb06cd4b7e5f572bb648827cd293d1f2dc
seeyeon <gulf72pr@swu.ac.kr>
2024-11-28 17:06:54 +0900

    setting

---
+++ src/main/java/com/ll/Calculator.java
@@ -0,0 +1,4 @@
+package com.ll;
+
+public class Calculator {
+}
---
+++ src/test/java/com/ll/CalculatorTest.java
@@ -0,0 +1,9 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+
+}
