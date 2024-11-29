876a044eecebd675630fc8c5cad5d57849d171ed
seeyeon <gulf72pr@swu.ac.kr>
2024-11-29 10:25:37 +0900

    t8 g

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,15 +1,26 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calc {
 
    public static int run(String expression) {
 
-      if(expression.contains("+")){
-         String[] expre = expression.split("\\+");
-         int num1 = Integer.parseInt(expre[0].trim());
-         int num2 = Integer.parseInt(expre[1].trim());
+      List<Integer> params = new ArrayList<>();
+      int sum =0;
 
-         return num1 + num2;
+      if(expression.contains("+")){
+
+         String[] numbers = expression.split(" \\+ ");
+         for(int i=0; i<numbers.length; i++){
+           params.add(Integer.parseInt(numbers[i].trim()));
+         }
+
+         for(int i : params){
+            sum += i;
+         }
+         return sum;
       }
 
       if(expression.contains("-")){
