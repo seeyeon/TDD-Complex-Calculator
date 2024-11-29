48f1e4577ffc0ebc825c471a31f25810b2506e43
seeyeon <gulf72pr@swu.ac.kr>
2024-11-29 09:10:42 +0900

    t4 r

--- src/test/java/com/ll/CalculatorTest.java
+++ src/test/java/com/ll/CalculatorTest.java
@@ -31,4 +31,10 @@ public class CalculatorTest {
         assertThat(Calc.run("1000 + 280")).isEqualTo(1280);
     }
 
+    @Test
+    @DisplayName("2 - 1 == 1")
+    void t5() {
+        assertThat(Calc.run("2 - 1")).isEqualTo(1);
+    }
+
 }
