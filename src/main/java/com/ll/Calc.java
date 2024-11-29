d6525c8ed606ded6e2d092fec3c8c95af3bf9ea8
seeyeon <gulf72pr@swu.ac.kr>
2024-11-29 10:12:41 +0900

    t5~7 g/b

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -4,12 +4,23 @@ public class Calc {
 
    public static int run(String expression) {
 
-      String[] expre = expression.split("\\+");
-      int num1 = Integer.parseInt(expre[0].trim());
-      int num2 = Integer.parseInt(expre[1].trim());
+      if(expression.contains("+")){
+         String[] expre = expression.split("\\+");
+         int num1 = Integer.parseInt(expre[0].trim());
+         int num2 = Integer.parseInt(expre[1].trim());
 
-      return num1 + num2;
+         return num1 + num2;
+      }
 
+      if(expression.contains("-")){
+         String[] expre = expression.split("\\-");
+         int num1 = Integer.parseInt(expre[0].trim());
+         int num2 = Integer.parseInt(expre[1].trim());
+
+         return num1 - num2;
+      }
+
+      return 0;
    }
 }
 
--- src/test/java/com/ll/CalculatorTest.java
+++ src/test/java/com/ll/CalculatorTest.java
@@ -37,4 +37,16 @@ public class CalculatorTest {
         assertThat(Calc.run("2 - 1")).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("3 - 1 == 2")
+    void t6() {
+        assertThat(Calc.run("3 - 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("100 - 20 == 80")
+    void t7() {
+        assertThat(Calc.run("100 - 20")).isEqualTo(80);
+    }
+
 }
